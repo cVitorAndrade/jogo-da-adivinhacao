497de157925e1b7f1316a3efd7218b0dffdbfe5c
cVitorAndrade <vitorandrade1804@gmail.com>
2022-12-08 18:24:06 -0300

    feat: adding second screen logic

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-const randomNumber = Math.round(Math.random() * 10)
-console.log(randomNumber)
+const screenOne = document.querySelector('.screen1')
+const screenTwo = document.querySelector('.screen2')
 
+const btnTry = document.querySelector('#btnTry')
+const btnAgain = document.querySelector('#btnPlayAgain')
+
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-function handleClick(event) {
-    const inputNumber = document.querySelector("#inputNumber")
-    console.log(inputNumber.value)
-    
+// Events
+
+btnTry.addEventListener('click', handleTryClick)
+btnAgain.addEventListener('click', handlePlayAgainClick)
+
+// Functions
+
+function handleTryClick(event) {
+    const inputNumber = document.querySelector("#inputNumber")    
 
     event.preventDefault()
     
     if(Number(inputNumber.value) == randomNumber){
-        document.querySelector('.screen1').classList.add("hide")
-        document.querySelector('.screen2').classList.remove("hide")
+        screenOne.classList.add("hide")
+        screenTwo.classList.remove("hide")
 
-        document.querySelector('h2').innerText = `Acertou em ${xAttempts} tentativa(s)`
+        document.querySelector('.screen2 h2').innerText = `Acertou em ${xAttempts} tentativa(s)`
     }else {
         xAttempts++
+        inputNumber.value = ''
     }
+}
+
+function handlePlayAgainClick() {
+    screenOne.classList.remove("hide")
+    screenTwo.classList.add("hide")
+
+    xAttempts = 1
+    inputNumber.value = ''
+    randomNumber = Math.round(Math.random() * 10)
 }
